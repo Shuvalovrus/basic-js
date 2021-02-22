67eae0fff081063bcc185bf7933336e149a2c7c7
Konstantin <shuvalov.2021@list.ru>
2021-02-22 19:59:55 +0300

    feat: add solution dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  "use strict"
+
+  if(Array.isArray(members) === false){
+    return false
+  } else if (members.length === 0){
+    return "FAIL"
+  }
+
+  let result = [];
+  let arr = members.filter(item => typeof item === "string").map(item => item.trim());
+
+  for(let i = 0; i < arr.length; i++) {
+    if(typeof arr[i] === "string"){
+    result.push(arr[i].charAt(0).toUpperCase());
+    }
+  }
+
+  return result.sort().join('');
+
+ };
